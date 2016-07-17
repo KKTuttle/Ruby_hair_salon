1bf98f3d70b2af1346ef23703b80759bf60d20c3
Katie Tuttle <katka.karolcikova@gmail.com>
2016-07-17 08:00:14 -0700

    Create stylists.erb, stylist.erb and success.erb

--- app.rb
+++ app.rb
@@ -6,3 +6,23 @@ also_reload('lib/**/*.rb')
 require("pg")
 
 DB = PG.connect({:dbname => "hair_salon"})
+
+get('/') do
+  erb(:index)
+end
+
+get('/stylists/new') do
+  erb(:stylist_form)
+end
+
+post('/stylists') do
+  name = params.fetch('name')
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  erb(:success)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
